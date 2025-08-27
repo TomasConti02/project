05298bdaeb1c19d2ca638f4a1efc67cc9ef4f7b1
TomasConti <tomasconti34@gmail.com>
2025-08-27 14:23:34 +0200

    aggiunto icona e chat html e css

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nuova-repo
+#pip install fastapi uvicorn jinja2
+
+launch -> uvicorn main:app --reload
+server open on http://127.0.0.1:8000
--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-app = FastAPI()
+#FastAPI → il framework web.
+#Request → oggetto che rappresenta la richiesta HTTP.
+app = FastAPI() #creo istanza 
 
 # cartelle statiche e template
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static") #indico dove si trovano i file css statici
+templates = Jinja2Templates(directory="templates") #indico file html della page 
 
+#Quando visiti http://localhost:8000/ con un GET, viene caricata la pagina index.html.
 # pagina iniziale con il form
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request):
+def home(request: Request): #all'arrivo della get su 8000/ viene eseguita operazione di home
     return templates.TemplateResponse("index.html", {"request": request, "result": None})
 
 # ricezione della stringa dal form
